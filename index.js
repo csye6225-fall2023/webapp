48a069c4bbff38ea9fd507be8311124975b9ffd1
Harshil Shah <shah.harshil2@northeastern.edu>
2023-11-25 17:56:27 -0500

    debug env

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import app from './api/app.js'; // Import the app
 import { syncDB } from './api/sequelize.js';
+import { logger } from './configs/logger.config.js';
 
 const port = process.env.PORT || 8080; // Set the port
 
 app.listen(port, async () => { // Start the server
     //sync the database
     await syncDB();
+
+    //print the .env to logger
+    logger.info(JSON.stringify(process.env, null, 2));
     
     console.log(`Server listening on port ${port}`);
 });
